2073e16874e969c291d0b7024432ff67ea28fd9f
lisongyu <269876940@qq.com>
2020-08-03 07:32:23 +0800

    提交props船只

--- src/PropsLearn.js
+++ src/PropsLearn.js
@@ -1,22 +1,36 @@
 import React, { Component } from 'react'
 
 
-class Header extends Component{
+class Header extends Component {
+  static defaultProps = { // 给props属性添加默认值
+    title: "skyblue"
+  }
+  // this.props.children 获取子元素
   render() {
     return (
       <header>
-        标题
+        {this.props.title}
+        {this.props.children}
       </header>
     )
   }
 }
 
 export default class PropsLearn extends Component {
-  
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      title: '首页标题'
+    }
+  }
+
   render() {
     return (
       <div>
-        <Header/>
+        <Header title={this.state.title} />
+        <Header title="列表页标题" />
+        <Header>子元素</Header>
       </div>
     )
   }
