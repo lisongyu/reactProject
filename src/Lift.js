6fa2104b3c6f1838b41a0be9005f1b91f0fc3032
lisongyu <269876940@qq.com>
2020-08-04 07:16:49 +0800

    提交生命周期代码更新期

---
+++ src/Lift.js
@@ -0,0 +1,34 @@
+// 4组件生命周期挂载期
+import React, { Component } from 'react'
+
+export default class Lift extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      num: 20
+    }
+    this.handleClick = this.handleClick.bind(this)
+    console.log('1.1 constructor 初始化组件')
+  }
+  handleClick() {
+    alert('点击按钮')
+  }
+  //WARNING! To be deprecated in React v17. Use componentDidMount instead.
+  UNSAFE_componentWillMount() {
+    console.log('1.2 在挂载数据之前需要执行的代码')
+  }
+  componentDidMount() {
+    console.log('1.4 在挂载数据之后需要执行的代码')
+    // 异步请求
+  }
+  render() {
+    console.log('1.3渲染标签到也买呢上，填充数据到标签中')
+    return (
+      <div>
+        <p>{this.state.num}</p>
+        <button onClick={this.handleClick}>点击按钮</button>
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './ChildToParent'
+import App from './Stateupdate'
 
 ReactDOM.render(
 
